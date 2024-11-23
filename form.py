4c045c2bff15bb1222a1a410b496d1728ca39235
fishtries <daniilkornilenko@gmail.com>
2024-11-23 23:08:21 +0300

    МИШАААААА

--- form.py
+++ form.py
@@ -1,22 +1,27 @@
+#обратная связь - history-2025@mail.ru
+
 import customtkinter as ctk
+
+# global selected_region
+# global selected_size
+# selected_region = None
+# selected_size = None
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
         self.page = 1
 
-        self.RegionInfo = ''
-        self.SizeInfo = ''
+        self.Tags = []
 
-        with open(r'datap.txt', 'w', encoding='utf-8') as data:
-            data.write(f"-r-{self.RegionInfo}\n")
-            data.write(f"-s-{self.SizeInfo}\n")
+        # with open(r'datap.txt', 'w', encoding='utf-8') as data:
+        #     data.write(f"-r-{selected_region}\n")
+        #     data.write(f"-s-{selected_size} \n")
 
-        self.geometry("500x500")
+        self.geometry("800x600")
         self.title("Привет")
         self.resizable(False, False)
 
-        # Конфигурация сетки
         for i in range(5):
             self.grid_rowconfigure(i, weight=1)
         for j in range(5):
@@ -50,12 +55,33 @@ class App(ctk.CTk):
             "Ямало-Ненецкий автономный округ"
         ]
         self.SizeList = [
-            'Малый', 'Средний', 'Большой', 'Микро'
+            'Микро', 'Малый', 'Средний'
+        ]
+        self.TypeBusinessList = [
+            'Индивидуальный предприниматель',
+            'Полное товарищество',
+            'Коммандитное товарищество',
+            'Общество с ограниченной ответственностью',
+            'Акционерное общество',
+            'Публичное акционерное общество',
+            'Кооператив'
+        ]
+        self.ListIDunno = [
+            'Денежная поддержка',
+            'Недвижимость',
+            'Обучение',
+            'Маркетинг и продвижение',
+            'Консультирование',
+            'Поддержка экспорта',
+            'Регистрация, разрешения, экспертиза',
+            'Выставка и ярмарки',
+            'Обеспечение хозяйственной деятельности'
         ]
 
-        self.create_welcome_widgets()
+        self.create_what_do_you_want()
 
     def create_welcome_widgets(self):
+        self.clear_widgets()
         self.text_welcome = ctk.CTkLabel(self, text='Привет. Введи свой регион:', text_color='gray')
         self.region_select = ctk.CTkComboBox(self, values=self.RegionList)
         self.region_done = ctk.CTkButton(self, text='Готово', command=self.on_done)
@@ -65,53 +91,79 @@ class App(ctk.CTk):
         self.region_done.grid(row=3, column=2, padx=10, pady=(15, 10), sticky='nsew')
 
     def create_another_widgets(self):
+        self.clear_widgets()
         self.choose_size_text = ctk.CTkLabel(self, text='Теперь выберите свой размер', text_color='gray')
-        self.size_select = ctk.CTkComboBox(self, values=self.SizeList)
+        self.size_select = ctk.CTkComboBox(self, values=self.SizeList,state='readonly')
 
         self.choose_size_text.grid(row=1, column=2, sticky='nsew')
         self.size_select.grid(row=2, column=2, pady=10, sticky='nsew')
         self.region_done.grid(row=3, column=2, pady=10, sticky='nsew')
 
-    def create_custom_tags(self):
-        self.text_existing_tags = ctk.CTkLabel(self, text='Ваши теги:')
-        self.tag_region = ctk.CTkButton(self, text=f'{self.RegionInfo}', state='disabled')
-        self.tag_size = ctk.CTkButton(self, text=f'{self.SizeInfo}', state='disabled')
-        self.all_done = ctk.CTkButton(self, text='Готово', command=self.on_done)
+    def chosen_news(self):
+        self.clear_widgets()
+        self.text_in_progress = ctk.CTkLabel(self, text='В процессе...', font=("Arial", 20, 'bold') )
+        self.text_in_progress.grid(row=2, column=2)
 
-        self.text_existing_tags.grid(row=1, column=2, pady=10, sticky='nsew')
-        self.tag_region.grid(row=2,column=1,pady=10,sticky='nsew')
-        self.tag_size.grid(row=2,column=3,pady=10,sticky='nsew')
-        self.all_done.grid(row=3,column=2,pady=10)
+    def create_what_do_you_want(self):
+        self.clear_widgets()
+        self.text_question = ctk.CTkLabel(self, text='Что вы хотите?', font=("Arial", 16, "bold"))
+        self.button_news = ctk.CTkButton(self, text='Хочу\nновости!', font=("Arial", 14, 'bold'), command=self.chosen_news)
+        self.button_recs = ctk.CTkButton(self, text='Хочу\nрекомендации!', font=("Arial", 14, 'bold'), command=self.create_welcome_widgets)
 
-    def show_me_reccomendations(self):
-        ...
+        self.text_question.grid(row=1, column=2, pady=10)
+        self.button_news.grid(row=2, column=1,padx=10,pady=10,sticky='nsew')
+        self.button_recs.grid(row=2, column=3,padx=10,pady=10,sticky='nsew')
+
+    def third_que_done(self):
+        self.Tag_text = self.third_que.get()
+
+        if (self.Tag_text != '') and (self.Tag_text not in self.Tags):
+            self.Tags.append(self.Tag_text)
+            self.third_done.configure(state='disabled', text='Введено!', fg_color='#247087')
+            self.third_que.configure(state='disabled')
+        else:
+            self.create_fourth_page()
+    def create_main_menu(self):
+        self.clear_widgets()
+
+        for i in range(10):
+            self.grid_rowconfigure(i, weight=1)
+        for j in range(5):
+            self.grid_columnconfigure(j, weight=1)
+
+        self.third_que = ctk.CTkEntry(self, width=300, height=30, placeholder_text='Производство бытовой электроники')
+        self.next_step= ctk.CTkButton(self, text='Готово', command=self.third_que_done)
+
+        self.fourth_que = ctk.CTkSwitch(self, text='Нет действующего бизнеса', font=("Arial", 20) )
+
+        ctk.CTkLabel(self, text='Ещё парочку вопросов...', font=("Arial", 30, 'bold')).grid(row=0, column=2)
+        ctk.CTkLabel(self, text='Какова ваша отрасль деятельности бизнеса?\n(не обязательно)', font=("Arial", 20)).grid(row=1, column=2)
+        self.third_que.grid(row=2, column=2)
+        self.next_step.grid(row=8, column=2)
+
+        self.fourth_que.grid(row=7, column=2)
+
+    def create_fourth_page(self):
+        self.clear_widgets()
+
+
     def on_done(self):
         if self.page == 1:
-            self.RegionInfo = self.region_select.get()
-            # with open(r'datap.txt',encoding='utf-8') as data:
-            #     for i in data:
-            #         if "-r-" in i:
-            #             self.RegionInfo = i[3:]
-
+            selected_region = self.region_select.get();
+            self.Tags.append(selected_region)
             self.page += 1
-            self.clear_widgets()
             self.create_another_widgets()
 
-        elif self.page == 2:
-            self.SizeInfo = self.size_select.get()
-            # with open(r'datap.txt',encoding='utf-8') as data:
-            #     for i in data:
-            #         if "-s-" in i:
-            #             self.SizeInfo = i[3:]
-            self.page += 1
-            self.clear_widgets()
-            self.create_custom_tags()
+            # with open(r'datap.txt', 'w', encoding='utf-8') as data:
+            #     data.write(f"-r-{selected_region}\n")
 
-        elif self.page == 3:
-            #pars pars pars
+        elif self.page == 2:
+            selected_size = self.size_select.get()
+            self.Tags.append(selected_size)
             self.page += 1
-            self.clear_widgets()
-            self.create_custom_tags()
+            self.create_main_menu()
+
+            # with open(r'datap.txt',
 
     def clear_widgets(self):
         for widget in self.winfo_children():
@@ -121,20 +173,3 @@ class App(ctk.CTk):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-"""
-region = "Кубань"
-napravlen = "Средний бизнес"
-otras = "Машины"
-with open(r'data.txt', 'w', encoding='utf-8') as data:
-   data.write(f"-r-{region}\n")
-   data.write(f"-n-{napravlen}\n")
-   data.write(f"-o-{otras}\n")
-with open(r'data.txt', encoding='utf-8') as data:
-   for i in data:
-      if "-r-" in i:
-         print(f"Регион {i[3:]}")
-      if "-n-" in i:
-         print(f"Направление {i[3:]}")
-      if "-o-" in i:
-         print(f"Отрасль {i[3:]}")
-"""
